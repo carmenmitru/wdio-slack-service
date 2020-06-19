e81f71d09f877c46ba244de2cb18443897657ddb
Mitru Carmen <carmen.mitru@gmail.com>
2020-06-19 20:19:51 +0300

    fix slack service
    - add option notifyOnlyOnFailureoption
    - add icon for error and text
    - displays total number of  tests/ failed/ passed
    - fix compatibility with v6

--- src/index.js
+++ src/index.js
@@ -1,39 +1,68 @@
 const { IncomingWebhook } = require("@slack/webhook");
-const failedColor = "#E51670";
+const failedColor = "#dc3545";
 class SlackService {
     constructor(config) {
         this.config = config;
+
+        this.failedTests = 0;
+        this.tests = 0;
         if (!this.config.webhook) {
-            console.error(
-                "[slack-reporter] Slack Webhook URL is not configured, notifications will not be sent to slack."
-            );
-            return;
+          console.error(
+            "[slack-reporter] Slack Webhook URL is not configured, notifications will not be sent to slack."
+          );
+          return;
         }
+    
         this.webhook = new IncomingWebhook(this.config.webhook);
-        this.message = this.config.message || "Running Tests Report";
+        this.message = this.config.message || "WebdriverIO Slack Reporter";
         this.attachments = [
-            {
-                pretext: `*${this.message}*`,
-            }
+          {
+            pretext: `*${this.message} *`,
+            title: "",
+          },
         ];
     }
 
-    afterTest(test) {
-        if (test.passed === false) {
-            let attach = {
-                color: failedColor,
-                author_name: test.fullTitle,
-                footer: test.error.stack,
-                ts: Date.now()
-            };
-            this.attachments.push(attach);
+     afterTest(test, context, results) {
+        this.tests++;
+
+        const { error, passed } = results;
+    
+        let testError = error.matcherResult
+          .message()
+          .replace(
+            /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
+            ""
+          );
+    
+    
+        let attach = {
+          color: failedColor,
+          author_name: test.fullName || `${test.parent} - ${test.title}`,
+          footer: testError.toString(),
+          footer_icon: "https://img.icons8.com/color/48/000000/info--v1.png",
+          ts: Date.now(),
+        };
+        if (!passed) {
+           this.failedTests++;
+           this.attachments.push(attach);
         }
     }
 
     async after() {
-        await this.webhook.send({
-            attachments: this.attachments
-        });
+        if (this.config.notifyOnlyOnFailure === true) {
+            this.attachments[0].title += `Total tests: ${
+              this.tests
+            } \n Total passed: ${this.tests - this.failedTests} \n Total failed: ${
+              this.failedTests
+            }`;
+
+      
+            await this.webhook.send({
+              attachments: this.attachments,
+            });
+        }
+       
     }
 }
 
